30b8c8878dd9a22c03a90729c3f24a9dc7665973
François <francois.baudouin@epitech.eu>
2022-07-09 14:43:17 +0200

    fix syntax of pointer funct

--- src/parser/Interpretor.hpp
+++ src/parser/Interpretor.hpp
@@ -22,23 +22,23 @@ class Interpretor {
   private:
     AbstractVM::Memory memory;
     std::shared_ptr<std::pair<AbstractVM::eOperandType, std::string>> _dataCommand;
-    std::unordered_map<std::string, std::function<void()>> _functPtr {
-        {"push", [this]{memory.push();}},
-        {"pop", [this]{memory.pop();}},
-        {"clear", [this]{memory.clear();}},
-        {"dup", [this]{memory.dup();}},
-        {"swap", [this]{memory.swap();}},
-        {"dump", [this]{memory.dump();}},
-        {"assert", [this]{memory.assert();}},
-        {"add", [this]{memory.add();}},
-        {"sub", [this]{memory.sub();}},
-        {"mul", [this]{memory.mul();}},
-        {"div", [this]{memory.div();}},
-        {"mod", [this]{memory.mod();}},
-        {"load", [this]{memory.load();}},
-        {"store", [this]{memory.store();}},
-        {"print", [this]{memory.print();}},
-        {"exit", [this]{memory.exit();}},
+    std::unordered_map<std::string, std::function<void()>> _functPtr{
+        {"push", [this]() { memory.push(); }},
+        {"pop", [this]() { memory.pop(); }},
+        {"clear", [this]() { memory.clear(); }},
+        {"dup", [this]() { memory.dup(); }},
+        {"swap", [this]() { memory.swap(); }},
+        {"dump", [this]() { memory.dump(); }},
+        {"assert", [this]() { memory.assert(); }},
+        {"add", [this]() { memory.add(); }},
+        {"sub", [this]() { memory.sub(); }},
+        {"mul", [this]() { memory.mul(); }},
+        {"div", [this]() { memory.div(); }},
+        {"mod", [this]() { memory.mod(); }},
+        {"load", [this]() { memory.load(); }},
+        {"store", [this]() { memory.store(); }},
+        {"print", [this]() { memory.print(); }},
+        {"exit", [this]() { memory.exit(); }},
     };
 };
 
